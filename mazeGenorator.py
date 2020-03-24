fd1c6c7713db07ab99e840cc4e6c774d3a29ee10
Commander-Sloth <60903378+Commander-Sloth@users.noreply.github.com>
2020-03-23 21:33:35 -0400

    Fix 2x2 Gaps

--- mazeGenorator.py
+++ mazeGenorator.py
@@ -39,6 +39,15 @@ for rows in range(ROWS):
 
 	mazeArray.append(thisRow)
 
+testArray = []
+for rows in range(ROWS):
+	thisRow = []
+
+	for cols in range(COLS):
+		thisRow.append(0)
+
+	testArray.append(thisRow)
+
 #Set up the Pygame window.
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption('Maze')
@@ -48,7 +57,7 @@ screen.fill(backgroudColour)
 # Reusable function to return desired text object, which can be displayed.
 def drawText(labelText, xPos, yPos):
 	font = pygame.font.Font('freesansbold.ttf', 16)
-	text = font.render(labelText, True, (255, 255, 255))
+	text = font.render(labelText, True, (255, 0, 255))
 	textRect = text.get_rect()
 	textRect.center = (xPos, yPos)
 	return screen.blit(text, textRect)
@@ -154,9 +163,32 @@ def seachForZeros():
 						break
 
 
-def displayStuff():
+def drawFixers():
 	lineSize = 3
 
+	lineMarg = 15
+	for row in range(ROWS):
+		for col in range(COLS):
+			UP = getNewCoords([col, row], [0, -1])
+			LEFT = getNewCoords([col, row], [-1, 0])
+			DOWN = getNewCoords([col, row], [0, 1])
+			RIGHT = getNewCoords([col, row], [1, 0])
+
+			if positionInArray([col+1, row+1]) and positionInArray(RIGHT) and positionInArray(DOWN):
+				if mazeArray[row+1][col+1] == mazeArray[row][col]:
+					if (mazeArray[DOWN[1]][DOWN[0]] == mazeArray[row][col] + 1 or mazeArray[DOWN[1]][DOWN[0]] == mazeArray[row][col] -1) and (mazeArray[RIGHT[1]][RIGHT[0]] == mazeArray[row][col] + 1 or mazeArray[RIGHT[1]][RIGHT[0]] == mazeArray[row][col] -1):
+						pygame.draw.line(screen, (255, 255, 255), (col*30 + 30 - lineMarg, row*30 + 30 + lineMarg), (col*30 + 30 + lineMarg, row*30 + 30 + lineMarg), lineSize)
+					# OR RGHT
+
+			if positionInArray([col-1, row+1]) and positionInArray(LEFT) and positionInArray(DOWN):
+				if mazeArray[row+1][col-1] == mazeArray[row][col]:
+					if (mazeArray[DOWN[1]][DOWN[0]] == mazeArray[row][col] + 1 or mazeArray[DOWN[1]][DOWN[0]] == mazeArray[row][col] -1) and (mazeArray[LEFT[1]][LEFT[0]] != mazeArray[row][col] + 1 or mazeArray[LEFT[1]][LEFT[0]] != mazeArray[row][col] -1):
+						pygame.draw.line(screen, (255, 255, 255), (col*30 + 30 - lineMarg, row*30 + 30 + lineMarg), (col*30 + 30 + lineMarg, row*30 + 30 + lineMarg), lineSize)
+					# OR RGHT
+
+
+def displayStuff():
+	lineSize = 3
 	lineMarg = 15
 	for row in range(ROWS):
 		for col in range(COLS):
@@ -188,14 +220,27 @@ def displayStuff():
 			pygame.draw.rect(screen, (255, 255, 255), (col*30 + 30 - lineMarg, row*30 + 30 - lineMarg, lineSize-1, lineSize-1)) #, lineSize*2
 
 			pygame.draw.rect(screen, (255, 255, 255), (15, 15, WIN_WIDTH-30 , WIN_HEIGHT-30), lineSize)
-			drawText(str(mazeArray[row][col]), col*30 + 30, row*30 + 30)
+			#drawText(str(mazeArray[row][col]), col*30 + 30, row*30 + 30)
+			#drawText(str(testArray[row][col]), col*30 + 30, row*30 + 30)
 
 
 def newBranch(coordinates, direction, number):
-	global mazeArray
+	global mazeArray, testArray
 	#print_array()
-
 	c, r = coordinates
+
+
+
+	if direction == [0, -1]:
+		testArray[r][c] = "|"
+	if direction == [0, 1]:
+		testArray[r][c] = "|"
+	if direction == [-1, 0]:
+		testArray[r][c] = "--"
+	if direction == [1, 0]:
+		testArray[r][c] = "--"
+
+	
 	if positionInArray(coordinates):
 		mazeArray[r][c] = number
 
@@ -218,12 +263,12 @@ def newBranch(coordinates, direction, number):
 				if testForVal(newCo):
 					newBranch(newCo, possibleDirections[1], number + 1)
 
-			if len(possibleDirections) > 2:
-				FiftyFiftyy = random.randint(0,3)
-				if FiftyFiftyy == 1:
-					newCoo = getNewCoords(coordinates, possibleDirections[1])
-					if testForVal(newCoo):
-						newBranch(newCoo, possibleDirections[1], number + 1)
+		# 	if len(possibleDirections) > 2:
+		# 		FiftyFiftyy = random.randint(0,3)
+		# 		if FiftyFiftyy == 1:
+		# 			newCoo = getNewCoords(coordinates, possibleDirections[1])
+		# 			if testForVal(newCoo):
+		# 				newBranch(newCoo, possibleDirections[1], number + 1)
 
 
 	else: # If I can't go anywhere
@@ -247,6 +292,8 @@ while any(0 in sublist for sublist in mazeArray):
 	newBranch([3,3], [0,1], 1)
 	#seachForZeros()
 
+#print(testArray)
+
 # Main loop to update and draw the game in screen.
 def gameLoop():
 	global notRunning
@@ -267,6 +314,7 @@ def gameLoop():
 		if timeTracker % 10 == 0:
 			screen.fill(backgroudColour)
 			displayStuff()
+			drawFixers()
 
 		pygame.display.flip()	
 		clock.tick(60)
